fe79645cd31bc411ecfd17175a4ca5c34e796d51
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-02-26 18:14:43 +0700

    Fixed: Numbers of Products in Cart to show all pages

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -8,5 +8,18 @@ module.exports = function(req, res, next) {
 		db.get('sessions').push({ id: sessionId }).write();
 	};
 
+	// Show numbers of products added to cart
+	var cart = db.get('sessions')
+		.find({ id: req.signedCookies.sessionId })
+		.get('cart')
+		.value();
+
+	var numbers = 0;
+	for (var product in cart) {
+		numbers += cart[product];
+	}
+
+	res.locals.numbers = numbers;
+
 	next();
 };
